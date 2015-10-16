da7a90eadd1197fb46ef5fa297f125f82ae124c7
Manon Cortial <mc@generationrobots.com>
2015-10-16 10:58:01 +0200

    added md2rst in make html, modified all index

--- md2rst.sh
+++ md2rst.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 find . -name '*.md' -print0 | while IFS= read -r -d '' file; do
-	pandoc --from=markdown --to=rst --output="${file%.md}.rst" "$file";
-	echo "$file" "->" "${file%.md}.rst";
+    if [[ $file = ./poppy-humanoid/poppy-humanoid* ]] || [[ $file = ./poppy-torso/poppy-torso* ]] || [[ $file = ./pypot/pypot* ]]  || [[ $file = ./poppy-ergo-jr/poppy-ergo-jr* ]]  || [[ $file = *README ]]; then
+        echo "$file" "not used"
+    else
+        pandoc --from=markdown --to=rst --output="${file%.md}.rst" "$file";
+        echo "$file" "->" "${file%.md}.rst";
+    fi
 done
--- poppy-humanoid/assembly_doc/index.rst
+++ poppy-humanoid/assembly_doc/index.rst
@@ -12,10 +12,10 @@ Assembly guide for a Poppy Humanoid or Poppy Torso robot
     :maxdepth: 2
 
     BOM.md
-    ../../poppy-torso/BOM.md
+    ../../poppy-torso/BOM.rst
     
-    head_assembly.md
-    trunk_assembly.md
-    arms_assembly.md
-    legs_assembly.md
+    head_assembly.rst
+    trunk_assembly.rst
+    arms_assembly.rst
+    legs_assembly.rst
 
