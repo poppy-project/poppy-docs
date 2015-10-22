7b406d93acfedfb801c886f00f4eb99a7440ef70
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-10-22 11:50:35 +0100

    Use the vectoriel poppy logo.

--- conf.py
+++ conf.py
@@ -150,7 +150,7 @@ html_theme = 'basicstrap'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'images/poppy_name_h144.png'
+html_logo = 'images/poppy_name.pdf'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
