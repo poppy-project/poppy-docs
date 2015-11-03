ce58767f8e1e96eb8ad13de64bae35af312af2a3
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-11-03 12:07:42 +0100

    Continue cleaning.
    Former-commit-id: 03807561ed74f74f6262d122dfee1ae2d6ab9aa3

--- md2rst.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-find . -name '*.md' -print0 | while IFS= read -r -d '' file; do
-    if [[ $file = ./poppy-humanoid/poppy-humanoid* ]] || [[ $file = ./poppy-torso/poppy-torso* ]] || [[ $file = ./pypot/pypot* ]]  || [[ $file = ./poppy-ergo-jr/poppy-ergo-jr* ]]  || [[ $file = *README ]]; then
-        echo "$file" "not used"
-    else
-        pandoc --from=markdown --to=rst --output="${file%.md}.rst" "$file";
-        echo "$file" "->" "${file%.md}.rst";
-    fi
-done
