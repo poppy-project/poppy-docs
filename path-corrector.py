6034de8fa1d30f8155bbdfae882dac0acd5f892b
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-11-03 14:09:27 +0100

    Add templates for build.

--- path-corrector.py
+++ path-corrector.py
@@ -6,5 +6,6 @@ import sys
 p = re.compile(ur'(!\[[\w ]+]\()(../)(img/([\w\-\_]+\/)*\w+\.\w+\))', re.DOTALL)
 subst = ur"\1\3"
 
-result = re.sub(p, subst, sys.stdin.read())
-sys.stdout.write(result)
+for line in sys.stdin:
+    result = re.sub(p, subst, line)
+    sys.stdout.write(result)
