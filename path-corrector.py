5d69b53341353aa1f569842c8c1dfbd026f87593
Pierre Rouanet <pierre.rouanet@gmail.com>
2015-11-13 13:36:45 +0100

    Fix the regex path fixator.
    Former-commit-id: 33b3a7111b7a93547f862d8639bea4447f6cec1f

--- path-corrector.py
+++ path-corrector.py
@@ -3,7 +3,7 @@
 import re
 import sys
 
-p = re.compile(ur'(!\[[\w ]+]\()(../)(img/([\w\-\_]+\/)*\w+\.\w+\))', re.DOTALL)
+p = re.compile(ur'(!\[[\w ]+]\()(../)(img/([\w\-\_]+\/)*[\w\-\_]+\.\w+\))', re.DOTALL)
 subst = ur"\1\3"
 
 for line in sys.stdin:
