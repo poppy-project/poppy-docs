9f6763678523ae369feca80ec33f65d0c6f2bd4b
Théo Segonds <theo.segonds@inria.fr>
2016-02-25 10:54:50 +0100

    sync to getting-started branch
    Former-commit-id: 400f9a15a03012df9a818db4165c32d5cfb32ff5

---
+++ ci/sync_getting-started-only.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# Copy SUMMURY.md from the getting-started-only branch to the master and push force to getting-started-only 
+
+last_commit_sha=$(git rev-parse --short HEAD)
+# git config --global user.email $GIT_EMAIL
+# git config --global user.name $GIT_NAME
+# git_url=https://$GH_TOKEN@github.com/$GH_USERNAME/$GH_REPO.git
+git_url=https://github.com/poppy-project/poppy-docs.git
+tmp_repo=/tmp/getting-started
+branch="getting-started-only"
+
+# Configure Git to push with GitHub Oauth token
+# git remote set-url origin $git_url
+
+git clone $git_url $tmp_repo
+    
+pushd $tmp_repo/en
+    # Fix the summary
+    wget https://raw.githubusercontent.com/poppy-project/poppy-docs/getting-started-only/en/SUMMARY.md
+    git commit -m "Auto commit after $last_commit_sha (travis build #$TRAVIS_BUILD_NUMBER)"
+    git push --force  --quiet origin $branch
+popd
+
+
+# 
