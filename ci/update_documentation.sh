e5f10eba1f775e2d51c2aa53cf33cd3f636cae6d
Théo Segonds <theo.segonds@inria.fr>
2016-02-25 11:32:16 +0100

    auto update getting-started
    Former-commit-id: a3120d292e4bc2152c71eca52504f14154a84659

--- ci/update_documentation.sh
+++ ci/update_documentation.sh
@@ -10,7 +10,7 @@ git_url=https://$GH_TOKEN@github.com/$GH_USERNAME/$GH_REPO.git
 # git remote set-url origin $git_url
 
 # Push the broken links
-git add contributing/broken_links.txt
+git add -A
 git commit -m "Update broken links after commit $last_commit_sha"
 git push --quiet origin git_url > /dev/null 2>&1
 
