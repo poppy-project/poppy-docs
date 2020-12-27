edbfd17da0d0810894e5b73154a1adf35a811f65
Yoan Mollard <ymollard@users.noreply.github.com>
2020-12-27 22:52:15 +0100

    Changed colors :)

--- ci/deploy-to-netlify.sh
+++ ci/deploy-to-netlify.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 die(){ echo "Coudn't $*" >&2; exit 1; }
 
-echo -e "\e[34mStarting update_documentation.sh\e[39m: install deps"
+echo -e "\e[36mStarting update_documentation.sh\e[39m: install deps"
 
 npm install -g gitbook-cli || die "install gitbook-cli"
 gitbook install || die "run gitbook install"
 
-echo -e "\e[34mupdate_documentation.sh\e[39m: build"
+echo -e "\e[36mupdate_documentation.sh\e[39m: build"
 
 gitbook build -d || die "Build the documentation in HTML"
 
-echo -e "\e[34mupdate_documentation.sh\e[32m: SUCCEEDED AND DEPLOYED!\e[39m"
+echo -e "\e[36mupdate_documentation.sh\e[32m: BUILD SUCCEEDED!\e[39m"
